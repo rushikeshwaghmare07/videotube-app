517693b49a68052a5d02198b96d10ce0947baea8
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-07-13 23:10:03 +0530

    Add health check endpoint

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// import routes
+import healthCheckRouter from "./routes/healthcheck.route.js";
+
+// routes
+app.use("/api/v1/healthCheck", healthCheckRouter);
+
 export { app }
