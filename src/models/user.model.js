22f8693a1eaef484ee64e93acb0bb79815d7b9b7
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-09-01 21:30:05 +0530

    Add password hashing logic using bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -33,18 +34,30 @@ const userSchema = new Schema(
     watchHistory: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Video"
-      }
+        ref: "Video",
+      },
     ],
     password: {
       type: String,
-      required: [true, "password is required"]
+      required: [true, "password is required"],
     },
     refreshToken: {
       type: String,
-    }
+    },
   },
   { timestamps: true }
-)
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
