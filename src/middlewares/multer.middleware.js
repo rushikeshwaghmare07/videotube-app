1da33ad3a655780c35066534256991728fc97f8d
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-09-01 22:21:57 +0530

    Add multer middleware for handling files

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadPath = `./public/temp`;
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // Only accept certain file types
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only JPEG and PNG files are allowed"), false);
+  }
+};
+
+export const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 }, // Limit file size to 5MB
+});
