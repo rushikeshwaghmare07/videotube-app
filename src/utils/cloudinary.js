c3d28da06d51854e263d5618182130d85edb9eb5
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-09-03 00:43:53 +0530

    feat: add `deleteFromCloudinary` function to handle file deletion on Cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -22,10 +22,20 @@ const uploadOnCloudinary = async (localFilePath) => {
     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
-    console.log("Error on Cloudinary: ", error)
+    console.log("Error on Cloudinary: ", error);
     fs.unlinkSync(localFilePath);
     return null;
   }
 };
 
-export { uploadOnCloudinary };
+const deleteFromCloudinary = async (publicId) => {
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    console.log("Deleted from cloudinary. public Id: ", publicId);
+  } catch (error) {
+    console.error("Error deleting from cloudinary: ", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
