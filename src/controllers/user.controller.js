3c544eca938fd47db6b33a59434b1b97a145209e
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-09-08 22:34:12 +0530

    Implement account details update functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -243,4 +243,22 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, req.user, "Current user details"));
 });
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullname, email } = req.body;
+
+  if (!fullname || !email) {
+    throw new ApiError(400, "Fullname and email required");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    { $set: { fullname, email: email } },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
+
 export { registerUser, loginUser, refreshAccessToken, logoutUser };
