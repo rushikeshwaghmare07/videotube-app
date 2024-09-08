0834253575f9f7794c0f15f2d9fb447fe6d3d5df
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-09-08 22:23:27 +0530

    Add get current user details endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -237,4 +237,10 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user details"));
+});
+
 export { registerUser, loginUser, refreshAccessToken, logoutUser };
